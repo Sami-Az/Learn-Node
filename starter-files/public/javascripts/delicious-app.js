a95e00337f92435ece71792d49aa865d6a3cb878
Sami Abderrezak <sami.abderre@gmail.com>
2018-09-30 14:00:16 +0100

    app

--- starter-files/public/javascripts/delicious-app.js
+++ starter-files/public/javascripts/delicious-app.js
@@ -1,3 +1,16 @@
 import '../sass/style.scss';
 
 import { $, $$ } from './modules/bling';
+import autocomplete from './modules/autocomplete';
+import typeAhead from './modules/typeAhead';
+import makeMape from './modules/map';
+import ajaxHeart from './modules/heart';
+
+autocomplete( $('#address'), $('#lat'), $('#lng'));
+
+typeAhead( $('.search') );
+
+makeMape( $('#map') );
+
+const heartForms = $$('form.heart');
+heartForms.on('submit', ajaxHeart);
